761f818fbc1b2a1e4747b47922837b1bba9a96b3
Tello- <lollisjosh@gmail.com>
2019-12-06 13:25:10 -0800

    Started experimenting with iteration

--- xmlParsing/main.cpp
+++ xmlParsing/main.cpp
@@ -52,38 +52,49 @@ int main()
 	rapidxml::file<> rxmlFile{ "xmlconfig.xml" }; // well now that i learned about this it makes my code.. useless
 	rapidxml::xml_document<> docFromFile;
 	docFromFile.parse<0>(rxmlFile.data());
-
-	std::string fileContents{ file.contents() };
-	char* cS;
-	int size = fileContents.size() + 1;
-	cS = new char[size];
-	strcpy_s(cS, size, fileContents.data());
 	
-
-	rapidxml::xml_document<> doc;
-	std::cout << "type of doc is: " << nodeTypeToString(doc.type()) << std::endl; // document
-	doc.parse<0>(cS);
-	std::cout << "\nFirst node of doc is: " << doc.first_node()->name() << "\n"; // font_pool
-	std::cout << "Type of " << doc.first_node()->name() << " is: " << nodeTypeToString(doc.first_node()->type()) << "\n"; // font_pool // element
-	std::cout << "\nchild of " << doc.first_node()->name() << " is: " << doc.first_node()->first_node()->name() << "\n"; // font_pool // font
-	std::cout << "Type of " << doc.first_node()->first_node()->name() << " is: " << nodeTypeToString(doc.first_node()->first_node()->type()) << "\n"; // font // element
-	std::cout << doc.first_node()->first_node()->name() << " has an attribute called " << doc.first_node()->first_node()->first_attribute()->name() << "\n";
-	std::cout << doc.first_node()->first_node()->first_attribute()->name() << " holds the value " << doc.first_node()->first_node()->first_attribute()->value() << "\n";
-	std::cout << "\n\n\n";
-
-	auto fontPool = doc.first_node(); 
-	auto firstFont = fontPool->first_node(); 
-	std::string firstFontTitle = firstFont->first_attribute()->value(); 
-	std::string firstFontKey = firstFont->first_node()->value(); 
-	std::string firstFontFilePath = firstFont->first_node()->next_sibling()->value(); 
+	// this works to iterate through all of the font nodes in the file but I'm not sure why i cant just get the
+	// iterator functionality to work in the rapidxml_iterators.h file
+	for (rapidxml::xml_node<> *nd = docFromFile.first_node()->first_node(); nd; nd = nd->next_sibling())
+	{
+		std::cout << "Fonts in file: " << nd->first_attribute()->value() << " " << std::endl;
+	}
 
 
-	std::cout << fontPool << std::endl; // address of font_pool
-	std::cout << firstFont << std::endl; // address of first_node of font_pool
-	std::cout << firstFontTitle << std::endl; // Nunito-Sans Bold
-	std::cout << firstFontKey << std::endl; // default_font
-	std::cout << firstFontFilePath << std::endl; // the file path of the font .. .. .. 
 
+	//std::string fileContents{ file.contents() };
+	//char* cS;
+	//int size = fileContents.size() + 1;
+	//cS = new char[size];
+	//strcpy_s(cS, size, fileContents.data());
+	//
+
+	//rapidxml::xml_document<> doc;
+	//std::cout << "type of doc is: " << nodeTypeToString(doc.type()) << std::endl; // document
+	//doc.parse<0>(cS);
+	//std::cout << "\nFirst node of doc is: " << doc.first_node()->name() << "\n"; // font_pool
+	//std::cout << "Type of " << doc.first_node()->name() << " is: " << nodeTypeToString(doc.first_node()->type()) << "\n"; // font_pool // element
+	//std::cout << "\nchild of " << doc.first_node()->name() << " is: " << doc.first_node()->first_node()->name() << "\n"; // font_pool // font
+	//std::cout << "Type of " << doc.first_node()->first_node()->name() << " is: " << nodeTypeToString(doc.first_node()->first_node()->type()) << "\n"; // font // element
+	//std::cout << doc.first_node()->first_node()->name() << " has an attribute called " << doc.first_node()->first_node()->first_attribute()->name() << "\n";
+	//std::cout << doc.first_node()->first_node()->first_attribute()->name() << " holds the value " << doc.first_node()->first_node()->first_attribute()->value() << "\n";
+	//std::cout << "\n\n\n";
+
+	//auto fontPool = doc.first_node(); 
+	//auto firstFont = fontPool->first_node(); 
+	//std::string firstFontTitle = firstFont->first_attribute()->value(); 
+	//std::string firstFontKey = firstFont->first_node()->value(); 
+	//std::string firstFontFilePath = firstFont->first_node()->next_sibling()->value(); 
+
+
+	//std::cout << fontPool << std::endl; // address of font_pool
+	//std::cout << firstFont << std::endl; // address of first_node of font_pool
+	//std::cout << firstFontTitle << std::endl; // Nunito-Sans Bold
+	//std::cout << firstFontKey << std::endl; // default_font
+	//std::cout << firstFontFilePath << std::endl; // the file path of the font .. .. .. 
+
+	
+	
 
 
 	return 0;
